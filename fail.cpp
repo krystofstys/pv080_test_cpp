cdf5a376f7c2179703d95045ae18d16bac3af162
Kryštof-Mikuláš Štys <83222554+krystofstys@users.noreply.github.com>
2021-05-10 14:27:57 +0200

    removed useless set of 0 outside of array boundaries

--- fail.cpp
+++ fail.cpp
@@ -7,18 +7,15 @@ int memcheckFailDemo(int* arrayStack, unsigned int arrayStackLen,
   Static[100] = 0; 
   Stack[100] = 0; 
 
-  for (int i = 0; i <= 5; i++) Stack [i] = 0;
+  for (int i = 0; i < 5; i++) Stack [i] = 0;
 
-  int* array = new int[5];
+  int* array = new int[101];
   array[100] = 0; 
 
-  arrayStack[100] = 0; 
-  arrayHeap[100] = 0; 
-
-  for (unsigned int i = 0; i <= arrayStackLen; i++) {
+  for (unsigned int i = 0; i < arrayStackLen; i++) {
       arrayStack[i] = 0;
   }
-  for (unsigned int i = 0; i <= arrayHeapLen; i++) {
+  for (unsigned int i = 0; i < arrayHeapLen; i++) {
       arrayHeap[i] = 0;
   }
 
