c1c6d1ccd1236400f151da6cac40bb5e2717a752
Kryštof-Mikuláš Štys <83222554+krystofstys@users.noreply.github.com>
2021-05-10 14:30:30 +0200

    removed weird accesses without purpose

--- fail.cpp
+++ fail.cpp
@@ -4,9 +4,6 @@ int memcheckFailDemo(int* arrayStack, unsigned int arrayStackLen,
 	int* arrayHeap, unsigned int arrayHeapLen) {
   int Stack[5];
 
-  Static[100] = 0; 
-  Stack[100] = 0; 
-
   for (int i = 0; i < 5; i++) Stack [i] = 0;
 
   int* array = new int[101];
